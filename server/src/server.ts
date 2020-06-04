6404e3bed468ff4346254fb9043ae9cca31182ab
LeoDalcegio <leoodalcegio@hotmail.com>
2020-06-04 07:02:37 -0300

    aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -9,6 +10,7 @@ const PORT = 3333 || process.env.PORT;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
